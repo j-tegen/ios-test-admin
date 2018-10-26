0b2888d172c5389d17fe03af8d161f8f733718a2
Jonatan Tegen <jonatan.tegen@gmail.com>
2018-10-26 16:52:10 +0200

    [payment type] - Add payment type

--- src/store/modules/payment_type.js
+++ src/store/modules/payment_type.js
@@ -35,11 +35,20 @@ const mutations = {
     state.loading = false
     state.error = ''
   },
+  addPaymentTypeSuccess: (state, payment_type) => {
+    state.error = ''
+    state.loading = false
+  },
+  editPaymentTypeSuccess: (state, payment_type) => {
+    state.error = ''
+    state.payment_types = state.payment_types.map(s => s._id === payment_type._id ? payment_type : s)
+    state.loading = false
+  },
 }
 
 const actions = {
-  fetchPaymentTypes: async ({ state, rootState, commit }) => {
-    if (state.payment_types.length > 0) {
+  fetchPaymentTypes: async ({ state, rootState, commit }, force) => {
+    if (!force && state.payment_types.length > 0) {
       return;
     }
     commit('beginCall')
@@ -56,7 +65,36 @@ const actions = {
     } catch(e) {
       commit('failedCall', e)
     }
-  }
+  },
+  addPaymentType: async ({ rootState, commit, dispatch}, payment_type) => {
+    commit('beginCall')
+    try {
+      const { token } = rootState.auth
+      const { data } = await api.add(token, payment_type)
+      if ( data.status === 'success' ) {
+        commit('addPaymentTypeSuccess')
+        dispatch('fetchPaymentTypes', true)
+      } else {
+        commit('failedCall', data.message)
+      }
+    } catch(e) {
+      commit('failedCall', e)
+    }
+  },
+  editPaymentType: async ({ rootState, commit}, payment_type) => {
+    commit('beginCall')
+    try {
+      const { token } = rootState.auth
+      const { data } = await api.editPaymentType(token, payment_type)
+      if ( data.status === 'success' ) {
+        commit('editPaymentTypeSuccess', data.data)
+      } else {
+        commit('failedCall', data.message)
+      }
+    } catch(e) {
+      commit('failedCall', e)
+    }
+  },
 }
 
 export default {
